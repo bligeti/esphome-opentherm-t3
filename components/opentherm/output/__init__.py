3c0405df06c7df72c5ec1f3baa159d7e00ff59fd
bligeti <64783210+bligeti@users.noreply.github.com>
2026-09-16 21:47:07 +0200

    Reorder import statements in __init__.py

--- components/opentherm/output/__init__.py
+++ components/opentherm/output/__init__.py
@@ -4,7 +4,8 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import output
 from esphome.const import CONF_ID
-from .. import const, schema, validate, input, generate
+
+from .. import const, generate, input, schema, validate
 
 DEPENDENCIES = [const.OPENTHERM]
 COMPONENT_TYPE = const.OUTPUT
